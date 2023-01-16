4972f2c71973087c2df9a2f83d603dcf292c88bd
小小源逼 <qa894178522@qq.com>
2023-01-16 12:49:57 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-/home/steam/l4d2/srcds_run -game left4dead2 -insecure +hostport 27015 +hostip 139.9.47.114 -condebug +map c1m1_hotel +exec server.cfg
+/home/steam/l4d2/srcds_run -game left4dead2 -insecure +hostport 27015 +hostip 139.9.47.114 -condebug -tickrate 60 +map c1m1_hotel +exec server.cfg
